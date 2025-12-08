14c682b95be30745b3d3b82e75df7dee60829bbd
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-12-08 13:25:48 +0000

    UPDATE

--- UBUNTU/install-vbox-additions.sh
+++ UBUNTU/install-vbox-additions.sh
@@ -11,5 +11,27 @@ else
     echo "bzip2 já está instalado!"
 fi
 
+if ! command -v gcc >/dev/null 2>&1; then
+    echo "Instalando gcc..."
+    sudo apt install -y gcc
+else
+    echo "gcc já está instalado!"
+fi
+
+if ! command -v make >/dev/null 2>&1; then
+    echo "Instalando make..."
+    sudo apt install -y make
+else
+    echo "make já está instalado!"
+fi
+
+if ! command -v perl >/dev/null 2>&1; then
+    echo "Instalando perl..."
+    sudo apt install -y perl
+else
+    echo "perl já está instalado!"
+fi
+
 sudo /media/VBoxLinuxAdditions-arm64.run
 
+#sudo reboot
