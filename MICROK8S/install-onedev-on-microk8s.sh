f6df52c2fe7a2512a8cc299dbb3d6d495c73fd23
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-12-27 14:02:57 +0000

    UPDATE

--- MICROK8S/install-onedev-on-microk8s.sh
+++ MICROK8S/install-onedev-on-microk8s.sh
@@ -15,3 +15,11 @@ microk8s helm3 install onedev onedev/onedev -n onedev --create-namespace \
 
 # Acesso local ao OneDev
 microk8s kubectl port-forward --namespace onedev svc/onedev 6610:80
+
+
+
+sudo microk8s stop
+sudo microk8s start
+sudo microk8s refresh-certs -e server.crt
+sudo microk8s refresh-certs -e ca.crt
+sudo microk8s refresh-certs -e front-proxy-client.crt
