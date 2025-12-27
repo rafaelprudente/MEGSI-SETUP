f599e900a8517b90715096d35d46293b0fa8b0c2
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-12-27 16:34:58 +0000

    UPDATE

--- AZURE/setup2.sh
+++ AZURE/setup2.sh
@@ -48,8 +48,13 @@ WORKDIR="$(pwd)/repos"
 mkdir -p "$WORKDIR"
 cd "$WORKDIR"
 
+rm -rf /repos/MEGSI-CONFIG-SERVER-DATA
 git clone https://github.com/rafaelprudente/MEGSI-CONFIG-SERVER-DATA.git
+
+rm -rf /repos/MEGSI-AUTENTICADOR
 git clone https://github.com/rafaelprudente/MEGSI-AUTENTICADOR.git
+
+rm -rf /repos/MEGSI-ITI-SERVICE-FILES
 git clone https://github.com/rafaelprudente/MEGSI-ITI-SERVICE-FILES.git
 
 echo "Copiando arquivos de configuração para /srv/configuration-server-fs..."
