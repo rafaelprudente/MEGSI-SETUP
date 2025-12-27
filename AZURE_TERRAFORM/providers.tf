261dbf3ecb06b2cda771d218b0b0f50988aa44df
Rafael R Prudente dos Santos <rafael.prudente.santos@gmail.com>
2025-12-27 22:41:51 +0000

    UPDATE

---
+++ AZURE_TERRAFORM/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
