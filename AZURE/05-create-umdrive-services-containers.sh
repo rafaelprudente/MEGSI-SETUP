5ca5f68b2c0f99ec2d7185c23195f778d5b074db
Rafael Prudente <32109908+rafaelprudente@users.noreply.github.com>
2025-12-28 22:19:08 +0000

    Update 05-create-umdrive-services-containers.sh

--- AZURE/05-create-umdrive-services-containers.sh
+++ AZURE/05-create-umdrive-services-containers.sh
@@ -2,6 +2,41 @@
 
 set -e
 
-echo "dckr_pat_nwSypHyWzV_SgLpYIBqm0b0mzFg" | docker login -u rafaelrpsantos --password-stdin
+sudo apt install openjdk-21-jdk -y
 
-docker compose -f compose-umdrive.yml up -d
+sudo rm -rf repos
+mkdir -p repos
+cd repos
+
+git clone https://github.com/rafaelprudente/MEGSI-CONFIG-SERVER-FS.git
+git clone https://github.com/rafaelprudente/MEGSI-AUTENTICADOR.git
+git clone https://github.com/rafaelprudente/MEGSI-ITI-SERVICE-FILES.git
+git clone https://github.com/rafaelprudente/MEGSI-ITI-AUTOSCALER.git
+git clone https://github.com/rafaelprudente/MEGSI-ITI-FRONTEND.git
+
+cd MEGSI-CONFIG-SERVER-FS
+mvn clean package
+sudo docker build -t "rafaelrpsantos/megsi-config-server-fs:latest" .
+cd ..
+
+cd MEGSI-AUTENTICADOR
+mvn clean package
+sudo docker build -t "rafaelrpsantos/megsi-autenticator:latest" .
+cd ..
+
+cd MEGSI-ITI-SERVICE-FILES
+mvn clean package
+sudo docker build -t "rafaelrpsantos/megsi-iti-service-files:latest" .
+cd ..
+
+cd MEGSI-ITI-AUTOSCALER
+sudo docker build -t "rafaelrpsantos/megsi-iti-autoscaler:latest" .
+cd ..
+
+cd MEGSI-ITI-FRONTEND
+sudo docker build -t umdrive-frontend .
+cd ..
+
+cd ~
+
+sudo docker compose -f compose-umdrive.yml up -d
